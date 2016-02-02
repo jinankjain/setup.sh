1261afaa9fa14344c0978b5c6c23b6e53ec34a5d
Jinank Jain <ug201210017@iitj.ac.in>
2016-02-02 20:35:56 +0530

    Mongo

--- setup.sh
+++ setup.sh
@@ -24,3 +24,10 @@ sudo apt-get install texlive-latex-recommended
 sudo apt-get install texlive-science
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+#Mongo DB
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
