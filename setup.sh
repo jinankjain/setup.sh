9bf872e6ac13eb497ce656cf8fcc0c775a277d6b
Jinank Jain <jinank94@gmail.com>
2022-03-28 09:13:56 +0000

    Add cargo utility to the list
    Signed-off-by: Jinank Jain <jinank94@gmail.com>

--- setup.sh
+++ setup.sh
@@ -16,12 +16,41 @@ function init () {
 function install_deb_if_not_present() {
 	if ! command -v "$2" &> /dev/null
 	then
-		echo "Installing $1"
+		echo "=================== Installing $1 ======================="
 		run_command "sudo apt-get install -y $1"
 	fi
 }
 
+function install_cargo_if_not_present() {
+	if ! command -v "$2" &> /dev/null
+	then
+		echo "=================== Installing $1 ======================="
+		run_command "cargo install $1"
+	fi
+}
+
+function install_fzf_if_not_present() {
+	if ! command -v "fzf" &> /dev/null
+	then
+		echo "=================== Installing $1 ======================="
+		run_command "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf"
+		run_command "~/.fzf/install"
+	fi
+}
+
 init
+
+# Things to be installed via package manager
 install_deb_if_not_present "gcc" "gcc"
 install_deb_if_not_present "g++" "g++"
+install_deb_if_not_present "git" "git"
 install_deb_if_not_present "python3-pip" "pip3"
+install_deb_if_not_present "tmux" "tmux"
+
+# Things to be install via cargo
+install_cargo_if_not_present "ripgrep" "rg"
+install_cargo_if_not_present "fd-find" "fdfind"
+install_cargo_if_not_present "zoxide" "z"
+install_cargo_if_not_present "zellij" "zellij"
+
+install_fzf_if_not_present
