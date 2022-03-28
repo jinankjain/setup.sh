06dc88014d6a8f993775f6c5bf5a8b6672a0116a
Jinank Jain <jinank94@gmail.com>
2022-03-28 08:51:25 +0000

    Add some basic commands which are required on all UNIX systems
    Signed-off-by: Jinank Jain <jinank94@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+function run_command () {
+	$1
+	if [ $? -ne 0 ]; then
+		echo "Failed to run $1"
+		exit 1
+	fi
+}
+
+function init () {
+	run_command "sudo apt-get update"
+	run_command "sudo apt-get upgrade"
+}
+
+function install_deb_if_not_present() {
+	if ! command -v "$2" &> /dev/null
+	then
+		echo "Installing $1"
+		run_command "sudo apt-get install -y $1"
+	fi
+}
+
+init
+install_deb_if_not_present "gcc" "gcc"
+install_deb_if_not_present "g++" "g++"
+install_deb_if_not_present "python3-pip" "pip3"
