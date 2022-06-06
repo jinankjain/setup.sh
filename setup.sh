4d19c8d4a848afbf820e778833d9a48fa3d58f6f
Jinank Jain <jinank94@gmail.com>
2022-06-06 09:56:23 +0530

    Fix an issue for golang in bashrc

--- setup.sh
+++ setup.sh
@@ -87,7 +87,7 @@ function install_golang_if_not_present() {
 		echo "=================== Installing golang ======================="
 		curl -OL "$go_bin_url"
 		run_command "sudo tar -C /usr/local -xvf go$go_version.linux-amd64.tar.gz"
-		echo "export PATH=$PATH:/usr/local/go/bin" >>"$HOME/.bashrc"
+		echo "export PATH=\"$PATH\":/usr/local/go/bin" >>"$HOME/.bashrc"
 		rm "go$go_version.linux-amd64.tar.gz"
 	fi
 }
